a8484ccc5c05dc74099bb5d2785c0f6cb59b5576
kosar <kosar@LAPTOP-140PAQBL>
2022-09-06 17:47:48 +0900

    修正

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -63,22 +63,20 @@ public class EmployeeAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
-
     }
-    /**
-     * 新規登録画面を表示する
-     * @throws ServletException
-     * @throws IOException
-     */
-    public void entryNew() throws ServletException, IOException {
+        /**
+         * 新規登録画面を表示する
+         * @throws ServletException
+         * @throws IOException
+         */
+        public void entryNew() throws ServletException, IOException {
 
-        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
-        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+            putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+            putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
 
-        //新規登録画面を表示
-        forward(ForwardConst.FW_EMP_NEW);
-    }
-
+            //新規登録画面を表示
+            forward(ForwardConst.FW_EMP_NEW);
+        }
 
     /**
      * 新規登録を行う
