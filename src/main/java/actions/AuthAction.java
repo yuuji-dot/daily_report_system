37779b296133695750ea01ec5f9995e5dfeb43c5
kosar <kosar@LAPTOP-140PAQBL>
2022-09-08 14:05:24 +0900

    ログインアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -15,37 +15,43 @@ import services.EmployeeService;
  * 認証に関する処理を行うActionクラス
  *
  */
-public class AuthAction extends ActionBase{
+public class AuthAction extends ActionBase {
+
     private EmployeeService service;
 
     /**
      * メソッドを実行する
      */
     @Override
-    public void process() throws ServletException, IOException{
+    public void process() throws ServletException, IOException {
+
         service = new EmployeeService();
+
         //メソッドを実行
         invoke();
-        service.close();
 
+        service.close();
     }
+
     /**
      * ログイン画面を表示する
      * @throws ServletException
      * @throws IOException
      */
-    public void showLogin() throws ServletException, IOException{
-      //CSRF対策用トークンを設定
-      putRequestScope(AttributeConst.TOKEN, getTokenId());
+    public void showLogin() throws ServletException, IOException {
 
-    //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
-      String flush = getSessionScope(AttributeConst.FLUSH);
-      if(flush != null) {
-          putRequestScope(AttributeConst.FLUSH,flush);
-          removeSessionScope(AttributeConst.FLUSH);
-      }
-      //ログイン画面を表示
-      forward(ForwardConst.FW_LOGIN);
+        //CSRF対策用トークンを設定
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+
+        //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH,flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //ログイン画面を表示
+        forward(ForwardConst.FW_LOGIN);
     }
     /**
      * ログイン処理を行う
@@ -90,6 +96,21 @@ public class AuthAction extends ActionBase{
             forward(ForwardConst.FW_LOGIN);
         }
     }
+    /**
+     * ログアウト処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void logout() throws ServletException, IOException {
+
+        //セッションからログイン従業員のパラメータを削除
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+
+        //セッションにログアウト時のフラッシュメッセージを追加
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+        //ログイン画面にリダイレクト
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+
+    }
 }
-
-
