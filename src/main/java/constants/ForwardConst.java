6b49677bb2f277cdbf6faf6b8f7e6dbe65304ec1
kosar <kosar@LAPTOP-140PAQBL>
2022-09-30 04:10:39 +0900

    ACT＿FOLの追加

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -12,6 +12,9 @@ public enum ForwardConst {
     ACT_EMP("Employee"),
     ACT_REP("Report"),
     ACT_AUTH("Auth"),
+    //action追加
+    ACT_FOL("Follow"),
+    //action追加ここまで
 
     //command
     CMD("command"),
@@ -27,6 +30,10 @@ public enum ForwardConst {
     CMD_UPDATE("update"),
     CMD_DESTROY("destroy"),
 
+    //command追加
+    CMD_TOP("top"),
+    //command追加ここまで
+
     //jsp
     FW_ERR_UNKNOWN("error/unknown"),
     FW_TOP_INDEX("topPage/index"),
@@ -38,7 +45,15 @@ public enum ForwardConst {
     FW_REP_INDEX("reports/index"),
     FW_REP_SHOW("reports/show"),
     FW_REP_NEW("reports/new"),
-    FW_REP_EDIT("reports/edit");
+    FW_REP_EDIT("reports/edit"),
+
+    //jsp追加
+    FW_FOL_INDEX("follows/index"),
+    FW_FOL_SHOW("follows/show"),
+    FW_FOL_NEW("follows/new"),
+    FW_FOL_EDIT("follows/edit");
+    //jsp追加ここまで
+
 
     /**
      * 文字列
